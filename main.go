a75e92ff92ddb778ce45af8bc90e387bb373454b
Aashish Upadhyay <aashishupadhyay@Aashishs-MacBook-Pro.local>
2023-08-21 10:11:51 -0400

    go web service

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello Gophers!")
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe("localhost:3000", nil)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	f, _ := os.Open("./menu.txt")
+	io.Copy(w, f)
 }
