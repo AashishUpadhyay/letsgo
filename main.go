55fa13dc0aa6e722c8a91800a360dfdeec01565e
Aashish Upadhyay <aashishupadhyay@Aashishs-MacBook-Pro.local>
2023-08-26 14:05:32 -0400

    looping

--- main.go
+++ main.go
@@ -5,15 +5,69 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	loopcollection()
+}
+
+func loop() {
+	i := 1
+	for {
+		fmt.Println(i)
+		i += 1
+		if i == 1000 {
+			break
+		}
+	}
+
+	for i < 1500 {
+		fmt.Println(i)
+		i += 1
+	}
+
+	for i := 1500; i < 1600; i += 1 {
+		fmt.Println(i)
+	}
+}
+
+func loopcollection() {
+	arr := [3]int{101, 102, 103}
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+	fmt.Println("Done!")
+
+	type menuItem struct {
+		name   string
+		prices map[string]float64
+	}
+
+	menu := []menuItem{
+		{name: "Caramel Machiato", prices: map[string]float64{"small": 1.65, "medium": 1.95, "large": 2.15}},
+		{name: "Penne Pollo", prices: map[string]float64{"half": 5.65, "full": 10.95, "double": 20.15}},
+	}
+
+	for _, item := range menu {
+		fmt.Println(item.name)
+		fmt.Println(strings.Repeat("-", 10))
+		for size, price := range item.prices {
+			fmt.Println(size + "-" + strconv.FormatFloat(price, 'E', -1, 64))
+		}
+	}
+}
+
+func webapp() {
+
 	fmt.Println("Hello Gophers!")
 	http.HandleFunc("/", Handler)
 	http.ListenAndServe("localhost:3000", nil)
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+
 	f, _ := os.Open("./menu.txt")
 	io.Copy(w, f)
 }
